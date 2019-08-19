e18dd0f52751a9688b63d90617cd254c23d7674a
WINDOWS-GV06GVP\Geoff <geoff@geoffmooremedia.com>
2019-08-18 20:43:20 -0400

    Lesson 3 complete

--- myApp.js
+++ myApp.js
@@ -13,7 +13,7 @@ console.log('Hello World');
 
 /** 2) A first working Express Server */
 app.get('/',(req,res)=>{
-    res.send('Hello Express');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 /** 3) Serve an HTML file */
